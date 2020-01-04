4f3d7168c84ee2e41c09b00d66796455ed594ec8
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-01-04 12:41:35 -0500

    jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello, My name is Larry</h1>
-      </div>
-    );
+     return (
+       <div className="App">
+         <h1>Hello, My name is Larry</h1>
+       </div>
+     );
+
+    //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hello, my name is Larry!!!'));
   }
 }
 
