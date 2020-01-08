2790345259acbebf6c221932c98f97b770915422
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-01-08 10:37:54 -0500

    add children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const person = (props) =>{
 
-    return <p>I'm {props.name} and I'm {props.age} years old.</p>
+    return( 
+        
+        <div>
+            <p>I'm {props.name} and I'm {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
+        
+
+    )
 }
 
 export default person;
